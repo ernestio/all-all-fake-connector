e4b910e25182028afcd65bc3db1ebba1a9cf9f36
Adria Cidre <adria.cidre@gmail.com>
2016-06-13 10:01:47 +0200

    Initial comit

---
+++ main.go
@@ -0,0 +1,122 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+
+	"encoding/json"
+
+	"github.com/nats-io/nats"
+)
+
+// Message is used to store any incoming messages
+type Message struct {
+	Type  string `json:"type"`
+	extra func() interface{}
+}
+
+// Transition is used to map a transitions arc, along with any extra information
+type Transition struct {
+	From  string
+	To    string
+	Extra string `json:"extra,omitempty"`
+}
+
+var natsURI string
+
+func getTransitions(path string) []Transition {
+	var keys []Transition
+
+	file, err := os.Open(path)
+	log.Printf("Reading config from: %s\n", path)
+	if err != nil {
+		log.Panic("error:", err)
+	}
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&keys)
+	if err != nil {
+		log.Println("Config file is invalid")
+		log.Panic("error:", err)
+	}
+
+	fmt.Printf("%#v\n", keys)
+	return keys
+}
+
+func manage(n *nats.Conn, m *nats.Msg, ts []Transition) {
+	var current Transition
+	for _, t := range ts {
+		if t.From == m.Subject {
+			current = t
+		}
+	}
+	fmt.Printf("%#v", current)
+	if &current != nil {
+
+		var x map[string]interface{}
+		err := json.Unmarshal(m.Data, &x)
+		if err != nil {
+			return
+		}
+		x["type"] = current.To
+
+		if current.Extra != "" {
+			var y map[string]interface{}
+			err = json.Unmarshal([]byte(current.Extra), &y)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("%#v", y)
+			for k, v := range y {
+				x[k] = v
+			}
+		}
+
+		b, err := json.Marshal(x)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+		log.Println("In")
+		n.Publish(current.To, b)
+	}
+
+}
+
+func main() {
+	trnPath := flag.String("transitions", "transitions/default.json", "path to json transitions")
+	flag.Parse()
+
+	if os.Getenv("NATS_URI") != "" {
+		natsURI = os.Getenv("NATS_URI")
+	} else {
+		natsURI = nats.DefaultURL
+	}
+
+	ts := getTransitions(*trnPath)
+	n, err := nats.Connect(natsURI)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	n.Subscribe("*", func(m *nats.Msg) {
+		manage(n, m, ts)
+	})
+
+	n.Subscribe("*.*", func(m *nats.Msg) {
+		manage(n, m, ts)
+	})
+
+	n.Subscribe("*.*.*", func(m *nats.Msg) {
+		manage(n, m, ts)
+	})
+
+	runtime.Goexit()
+}
